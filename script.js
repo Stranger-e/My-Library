2d44035e265fedfc2ae858ecfeaf66736b44a811
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-04-04 13:46:01 +0300

    Add a backdrop

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ closeModal.addEventListener('click', (event) => {
     event.preventDefault();
 })
 
+modal.addEventListener('click', e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if(
+        e.clientX < dialogDimensions.left || 
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ){
+        modal.close()
+    }
+});
+
 const myLibrary = [];
 
 
