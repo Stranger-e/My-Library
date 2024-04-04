4548166ea6bd3b8389222c39ae9ba436be3433cf
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-04-04 11:18:23 +0300

    Store user input in myLibrary array

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ closeModal.addEventListener('click', (event) => {
     modal.close()
     event.preventDefault();
 })
+
+const myLibrary = [];
+
+
+function Book(title,author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(event){
+    event.preventDefault();
+
+    const form = event.target;
+    const title = form.elements.title.value;
+    const author = form.elements.author.value;
+    const pages = form.elements.pages.value;
+
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+    form.reset();
+    modal.close();
+}
+
+document.querySelector('form').addEventListener('submit', addBookToLibrary);
